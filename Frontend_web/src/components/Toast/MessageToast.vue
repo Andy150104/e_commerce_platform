22d5e8f6bb4cc32468ff3ccfea877b845c05f4b8
Andy150104 <tranduyanh7766@gmail.com>
2025-02-10 09:48:48 +0700

    [UPDATE][FE][CONFIG] Add notification message, Fix NavBar

---
+++ Frontend_web/src/components/Toast/MessageToast.vue
@@ -0,0 +1,42 @@
+<template>
+  <UNotifications :ui="{ base: 'absolute top-0 end-0 start-0 h-1' }" />
+</template>
+<script setup lang="ts">
+  import { useFormMessageStore } from '@PKG_SRC/stores/master/formMessageStore'
+
+  const toast = useToast()
+  const formMessageStore = useFormMessageStore()
+
+  onMounted(() => {
+    if (formMessageStore.isAction) {
+      toast.add({
+        id: formMessageStore.messageId,
+        title: 'Message',
+        description: 'It will be installed on restart. Restart now?',
+        icon: 'i-heroicons-check-badge',
+        timeout: 1200,
+        ui: {
+          process: {
+            base: 'absolute top-0 end-0',
+          },
+        },
+      })
+    }
+    toast.add({
+      id: formMessageStore.messageId,
+      title: 'Message',
+      description: 'It will be installed on restart. Restart now?',
+      icon: 'i-heroicons-check-badge',
+      timeout: 1200,
+      ui: {
+        base: 'absolute top-0 end-0',
+      },
+      actions: [
+        {
+          label: 'Restart',
+          click: () => {},
+        },
+      ],
+    })
+  })
+</script>
