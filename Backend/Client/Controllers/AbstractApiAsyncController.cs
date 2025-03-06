dcf0648538dd89a281716ce24c061877d668e9ae
giakoii <khoipgse172073@fpt.edu.vn>
2025-03-06 11:30:56 +0700

    [Add][SourceCode] Add LoggingUtil class

--- Backend/Client/Controllers/AbstractApiAsyncController.cs
+++ Backend/Client/Controllers/AbstractApiAsyncController.cs
@@ -1,5 +1,6 @@
 using Client.Models.Helper;
 using Client.SystemClient;
+using Client.Utils;
 using Client.Utils.Consts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -50,18 +51,20 @@ public abstract class AbstractApiAsyncController<T, U, V> : ControllerBase
     /// <returns></returns>
     protected async Task<U> Post(T request, AppDbContext appDbContext, Logger logger, U returnValue)
     {
+        var loggingUtil = new LoggingUtil(logger, appDbContext?.IdentityEntity?.UserName ?? "System");
+
         // Get identity information 
         appDbContext.IdentityEntity = _identityApiClient?.GetIdentity(User);
-        logger.Warn(request);
+        loggingUtil.StartLog(request);
 
         // Check authentication information
         if (appDbContext.IdentityEntity == null)
         {
             // Authentication error
-            logger.Fatal($"Authenticated, but information is missing.");
+            loggingUtil.FatalLog($"Authenticated, but information is missing.");
             returnValue.Success = false;
             returnValue.SetMessage(MessageId.E11006);
-            logger.Warn(returnValue);
+            loggingUtil.EndLog(returnValue);
             return returnValue;
         }
         // Additional user information
@@ -74,10 +77,10 @@ public abstract class AbstractApiAsyncController<T, U, V> : ControllerBase
         catch (Exception e)
         {
             // Additional user information error
-            logger.Error($"Failed to get additional user information.：{e.Message}");
+            loggingUtil.FatalLog($"Failed to get additional user information.：{e.Message}");
             returnValue.Success = false;
             returnValue.SetMessage(MessageId.E11006);
-            logger.Warn(returnValue);
+            loggingUtil.EndLog(returnValue);
             return returnValue;
         }
 
@@ -98,11 +101,11 @@ public abstract class AbstractApiAsyncController<T, U, V> : ControllerBase
         }
         catch (Exception e)
         {
-            return AbstractFunction<T, U, V>.GetReturnValue(returnValue, logger, e, appDbContext);
+            return AbstractFunction<T, U, V>.GetReturnValue(returnValue, loggingUtil, e, appDbContext);
         }
 
         // Processing end log
-        logger.Warn(returnValue);
+        loggingUtil.EndLog(returnValue);
         return returnValue;
     }
 }
--- Backend/Client/Controllers/AbstractFunction.cs
+++ Backend/Client/Controllers/AbstractFunction.cs
@@ -19,28 +19,28 @@ public class AbstractFunction<T, U, V>
     /// <summary>
     /// Return value
     /// </summary>
-    public static U GetReturnValue(U returnValue, Logger logger, Exception e, AppDbContext context)
+    public static U GetReturnValue(U returnValue, LoggingUtil loggingUtil, Exception e, AppDbContext context)
     {
         switch (e)
         {
             case AggregateException:
-                logger.Warn($"Report API connection error ：{e.Message}");
+                loggingUtil.FatalLog($"Report API connection error ：{e.Message}");
                 returnValue.SetMessage(MessageId.E99001);
                 break;
             case DbUpdateConcurrencyException:
-                logger.Error($"Exclusive error ：{e.Message}");
+                loggingUtil.ErrorLog($"Exclusive error ：{e.Message}");
                 returnValue.SetMessage(MessageId.E99002);
                 break;
             case InvalidOperationException:
                 if (e.InnerException?.HResult == -2146233088)
                 {
                     // Exclusive control error Another update in the transaction
-                    logger.Error($"Exclusive error ：{e.Message}");
+                    loggingUtil.ErrorLog($"Exclusive error ：{e.Message}");
                     returnValue.SetMessage(MessageId.E99002);
                 }
                 else
                 {
-                    logger.Warn($"A system error has occurred.：{e.Message} {e.StackTrace} {e.InnerException}");
+                    loggingUtil.FatalLog($"A system error has occurred.：{e.Message} {e.StackTrace} {e.InnerException}");
                     returnValue.SetMessage(MessageId.E99999);
                 }
 
@@ -50,32 +50,32 @@ public class AbstractFunction<T, U, V>
                 if (e.InnerException?.HResult == -2147467259)
                 {
                     // SQL timeout
-                    logger.Error($"SQL timeout error ：{e.Message} {e.StackTrace} {e.InnerException}");
+                    loggingUtil.ErrorLog($"SQL timeout error ：{e.Message} {e.StackTrace} {e.InnerException}");
                     returnValue.SetMessage(MessageId.E99003);
                 }
                 else if (ex.Number == 3961)
                 {
                     // View definition changed
-                    logger.Warn(
+                    loggingUtil.ErrorLog(
                         $"The definition of a view, etc. was changed during processing ：{e.Message} {e.StackTrace} {e.InnerException}");
                     returnValue.SetMessage(MessageId.E99004);
                 }
                 else
                 {
-                    logger.Warn($"A system error has occurred ：{e.Message} {e.StackTrace} {e.InnerException}");
+                    loggingUtil.ErrorLog($"A system error has occurred ：{e.Message} {e.StackTrace} {e.InnerException}");
                     returnValue.SetMessage(MessageId.E99005);
                 }
 
                 break;
             case Exception:
                 // System error
-                logger.Warn($"A system error has occurred ：{e.Message} {e.StackTrace} {e.InnerException}");
+                loggingUtil.ErrorLog($"A system error has occurred ：{e.Message} {e.StackTrace} {e.InnerException}");
                 returnValue.SetMessage(MessageId.E99999);
                 break;
         }
 
         returnValue.Success = false;
-        logger.Warn(returnValue);
+        loggingUtil.EndLog(returnValue);
         return returnValue;
     }
 
